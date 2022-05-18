cdf086cca1a961d701c32e58291ac1c72f66d2aa
furPedroRibeiro <84580101+furPedroRibeiro@users.noreply.github.com>
2022-05-18 10:47:52 -0300

    Projeto em andamento dia 1

---
+++ conexao.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    $server = 'localhost';
+    $usuario = 'root';
+    $pass = '';
+    $bd = 'projeto';
+    $con = new mysqli($server,$usuario,$pass,$bd);
+    if(!$con){
+        echo "Erro na conexão: ".$con->error;
+    }
+    function Proteger(){
+        if(!$_SESSION['logado']){
+            vai('index.php');
+        }
+    }
+    function msg($texto){
+        echo '<script>M.toast({html: "'.$texto.'"});</script>';
+    }
+    function  vai($pagina){
+        echo '<script>window.location = "'.$pagina.'";</script>';
+    }
+?>
